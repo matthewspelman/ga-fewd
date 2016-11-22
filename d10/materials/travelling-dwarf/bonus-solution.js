22700df971463d6a77d9e33021327205eef01a9f
matthewspelman <matthewspelman@gmail.com>
2016-11-22 18:33:32 -0500

    d10 materials and hw submission

---
+++ d10/materials/travelling-dwarf/bonus-solution.js
@@ -0,0 +1,46 @@
+//d.orient('right'); // also 'right', 'up', and 'down'
+//d.move();
+
+var x = 0;
+var y = 0;
+
+g.at(x, y)
+
+//Is the next square empty? 
+//If so, then move? If not, then what direction can I turn? 
+//Then once I understand what direction is possible, then find out the orientation.
+//draw a decision tree// 
+//'d.orientation'
+//d.y
+
+
+//HINT: `g.at(x, y)` will tell you if there is an obstacle at the coordinate you specify...
+
+`d.orientation`
+`d.x`
+//`d.y`// //comment out the original solution...
+
+
+javascript
+
+// OR
+
+//var myName = "Jeff";
+
+//if (myName === "Rob" || myName === "Jeff") {
+ // alert("You are in charge of running FEWD!");
+//} else {
+//  alert("You are a student!");
+}
+
+
+// AND
+
+//var borough = "Manhattan";
+//var commuteTime = 30;
+
+//if (borough === "Manhattan" && commuteTime > 30) {
+//  alert("Maybe you should just live in Brooklyn or Queens...");
+//} else {
+//  alert("Cool - your spot in Manhattan is pretty commutable.");
+}
