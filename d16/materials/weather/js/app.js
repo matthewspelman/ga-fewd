3e370c901359005b6a42bc1e0e64e806725aa6d7
matthewspelman <matthewspelman@gmail.com>
2016-12-08 21:35:03 -0500

    d16

--- d16/materials/weather/js/app.js
+++ d16/materials/weather/js/app.js
@@ -1,7 +1,8 @@
 
 $("button").on("click", function() {
-	$("text").data();
-	$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=New%20York,NY&units=imperial&apikey=dfa6d8e9deab2a0cb7d4f15dc45b971d", function(data) {
-	+ "city" + "state" + "&";
-	  console.log(data);
+	var city = $("input").val();
+	$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&apikey=dfa6d8e9deab2a0cb7d4f15dc45b971d", function(data) {
+		$("h4 .temperature").text(data["main"]["temp"]);
+		$("img.icon").attr("src", "http://openweathermap.org/img/w/" + data["weather"][0]["icon"] +".png");
+	});
 });
